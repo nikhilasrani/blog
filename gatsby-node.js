b9f524f98d88e0486c25c59638f8b99874a3f7b7
Nikhil Asrani <asrani.nikhil@gmail.com>
2019-02-27 04:17:07 +0530

    removed commented code

--- gatsby-node.js
+++ gatsby-node.js
@@ -41,30 +41,4 @@ exports.createPages = ({ graphql, actions }) => {
       })
     )
   })
-  /* const postTemplate = path.resolve("src/templates/blog-post.js")
-
-  return graphql(`
-allMarkdownRemark {
-    edges {
-      node {
-        html
-        id
-        frontmatter {
-          path
-          title
-          date
-          author
-        }
-      }
-    }
-  }`).then(res => {
-    if (res.errors) return Promise.reject(res.erorrs)
-
-    res.data.allMarkdownRemark.edges.forEach(({ node }) => {
-      createPage({
-        path: node.frontmatter.path,
-        component: postTemplate,
-      })
-    })
-  }) */
 }
