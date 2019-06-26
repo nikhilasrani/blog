c61a1349fff1b58c7d1f2df3ae0e8f0c476133c8
Nikhil Asrani <asrani.nikhil@gmail.com>
2019-06-26 13:05:32 +0530

    added backlog project to the portfolio

--- src/pages/projects.js
+++ src/pages/projects.js
@@ -4,6 +4,7 @@ import SEO from "../components/seo"
 import Header from "../components/header/header"
 import Footer from "../components/footer/footer"
 import Project from "../components/Project/Project"
+import BacklogPortfolio from "../images/Backlog_Portfolio.png"
 import ThinderPortfolio from "../images/Thinder_Portfolio.png"
 import PortfolioPage from "../images/Portfolio_Page.png"
 const ProjectsPage = () => (
@@ -27,8 +28,14 @@ const ProjectsPage = () => (
           </a>
           .
         </p>
-        
-    <Project title="Backlog" subtitle="May 2019" 
+        <Project title="Backlog" subtitle="June 2019" 
+    description="Backlog is an application that allows you to save URLs or links that you want to refer back to later. It stores all kinds of URLs and displays them as cards containing information related to that link. Once you add a link using the Add button in the navigation bar, the link and its data gets stored in the applications cloud based servers. Everytime you open the application on the same or any different device logged onto your unique account, you will be able to see your saved links."
+     imageURL={BacklogPortfolio}
+     imageURLAlt="Backlog Portfolio Image"
+    // android 
+    // playStoreURL={"https://play.google.com/store/apps/details?id=com.nikhilasrani.thinder&hl=en"}
+     />
+    <Project title="Thinder" subtitle="May 2019" 
     description="Thinder is an application that helps you decide on a place to grab a quick bite when you are feeling indecisive. After selecting your location, you will see the restaurants in and around your area and you can swipe left or right on them on whether you like them or not. Once you have a few options down, you can see them on the Saved Restaurants screen and check them out further on Zomato."
      imageURL={ThinderPortfolio}
      imageURLAlt="Thinder Portfolio Image"
