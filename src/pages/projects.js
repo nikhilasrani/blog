ba089f007cb4e9240db725d31b7c97cc3ac99e44
Nikhil Asrani <asrani.nikhil@gmail.com>
2019-06-27 16:28:31 +0530

    added backlog and thinder URLs

--- src/pages/projects.js
+++ src/pages/projects.js
@@ -32,8 +32,8 @@ const ProjectsPage = () => (
     description="Backlog is an application that allows you to save URLs or links that you want to refer back to later. It stores all kinds of URLs and displays them as cards containing information related to that link. Once you add a link using the Add button in the navigation bar, the link and its data gets stored in the applications cloud based servers. Everytime you open the application on the same or any different device logged onto your unique account, you will be able to see your saved links."
      imageURL={BacklogPortfolio}
      imageURLAlt="Backlog Portfolio Image"
-    // android 
-    // playStoreURL={"https://play.google.com/store/apps/details?id=com.nikhilasrani.thinder&hl=en"}
+     android 
+     playStoreURL={"https://play.google.com/store/apps/details?id=com.nikhilasrani.backlog"}
      />
     <Project title="Thinder" subtitle="May 2019" 
     description="Thinder is an application that helps you decide on a place to grab a quick bite when you are feeling indecisive. After selecting your location, you will see the restaurants in and around your area and you can swipe left or right on them on whether you like them or not. Once you have a few options down, you can see them on the Saved Restaurants screen and check them out further on Zomato."
---
+++ src/pages/thinder.js
@@ -0,0 +1,12 @@
+import React, {Component} from "react";
+
+class thinder extends Component {
+render(){
+    return <div></div>
+}
+componentDidMount() {
+    window.location.replace("https://play.google.com/store/apps/details?id=com.nikhilasrani.thinder");
+  }
+}
+
+export default thinder;
